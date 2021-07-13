c83d9797436d7785c1a2337ec158c763c70467f1
Tyler Sorensen <tyler.sorensen@outlook.com>
2021-07-12 21:54:50 -0700

    updating scripts

--- OOPLSA_graphs/lobe_tightness/check.py
+++ OOPLSA_graphs/lobe_tightness/check.py
@@ -1,10 +1,10 @@
+import sys
 import os
 import pdb
 import re
 
-base_path = "../../Driver_and_Comparator_Results/"
-base_path2 = "../../extra_results/"
-
+empirical_base_path = "../../empirical_results/"
+formal_base_path = "../../Driver_and_Comparator_Results/"
 
 devices = ["GeForce 940M", "Quadro RTX 4000", "A14", "A12", "Mali-G77", "Adreno 620", "Tegra X1", "Intel HD620"]
 
@@ -12,23 +12,16 @@ device_alias = {"Quadro RTX 4000": "CUDA/toucan_quadro_rtx4000-inter-workgroup.c
                 "GeForce 940M" : "CUDA/laptop_940m-inter-workgroup.csv",
                 "A14" : "Apple/iphone_12_a14-inter-workgroup.csv",
                 "A12" : "Apple/ipad_air_3_a12-inter-workgroup.csv",
-                "Mali-G77": "galaxy-s20-mali-2021-02-07",
-                "Adreno 620" : "pixel5-adreno-2021-02-06",
-                "Tegra X1" : "results-NVIDIA-SHIELD-Android-TV",
-                "Intel HD620": "intelHD620-2021-03-16"}
-
+                "Mali-G77": "Vulkan/galaxy-s20-mali-2021-02-07.csv",
+                "Adreno 620" : "Vulkan/pixel5-adreno-2021-02-06.csv",
+                "Tegra X1" : "Vulkan/results-NVIDIA-SHIELD-Android-TV.csv",
+                "Intel HD620": "Vulkan/intelHD620-2021-03-16.csv"}
 
 config = ["2_thread_2_instruction",
           "2_thread_3_instruction",
           "2_thread_4_instruction",
           "3_thread_3_instruction",
           "3_thread_4_instruction"]
-
-config_alias = {"2_thread_2_instruction" : "output0",
-                "2_thread_3_instruction" : "output1",
-                "2_thread_4_instruction" : "output2",
-                "3_thread_3_instruction" : "output3",
-                "3_thread_4_instruction" : "output4"}
 
 
 def get_data(fname):
@@ -39,7 +32,7 @@ def get_data(fname):
 
 def find_ff_d_tests(c):
     ret = []
-    p = os.path.join(base_path,c,"schedulers/LOBE_STRONG_results.csv")
+    p = os.path.join(formal_base_path,c,"schedulers/LOBE_STRONG_results.csv")
     data = get_data(p).split("\n")[1:-2]
     for t in data:
         if "F" in t:
@@ -63,15 +56,8 @@ def check_res(res):
     return "N"
 
 def get_csv_path(d,c):
-    if d in ["Quadro RTX 4000", "A14", "A12", "GeForce 940M"]:
-        da = device_alias[d]
-        return os.path.join(base_path,c,da)
-    else:
-        da = device_alias[d]
-        dr = config_alias[c]
-        # maybe name has different date for different chips?
-        fname = "iteration_based_final_results.csv"
-        return os.path.join(base_path2,da,dr,fname)
+    da = device_alias[d]
+    return os.path.join(empirical_base_path,c,da)
 
 def split_d(d):
     d = d.split("\n")
@@ -81,25 +67,41 @@ def split_d(d):
     assert("Test File" in d[0])
     return d[1:-1]
 
+verbose = False
+if "-v" in sys.argv:
+    verbose = True
 
-for c in config:
-    r = find_ff_d_tests(c)
-    for d in devices:
-        c_d = 0
-        c_n = 0
+for d in devices:
+    print("Checking device:",d)
+    c_d = 0
+    c_n = 0
+    c_p = 0
+    for c in config:
+        r = find_ff_d_tests(c)
         p = get_csv_path(d,c)
         data = split_d(get_data(p))
         for t in r:
             v = check_res(data[t])
-            if v == "N":                
-                print("non-det:",c,d,t)
+            if v == "N":
+                if verbose:
+                    print("found non-deterministic passing test")
+                    print("config:",c)
+                    print("id:",t)
+                    print("--")                    
                 c_n += 1                
             if v == "P":
-                print("passed:",c,d,t)
+                if verbose: 
+                    print("found deterministic passing test")
+                    print("config:",c)
+                    print("id:",t)
+                    print("--")                    
+                c_p += 1
             if v == "D":
                 c_d += 1
-
-        print("total:", d, c_d, c_n)
+    
+    print("total passed deterministically:", c_p)
+    print("total passed non-deterministically:", c_n)
+    print("------------")
             
         
     
